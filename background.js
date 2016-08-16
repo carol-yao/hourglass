9f1912f6b130b38d84c810ee8363ba4b64f8616d
Carol Yao <c.y-@hotmail.com>
2016-08-15 21:56:15 -0400

    made popup and appended text into popup for the time to read for body of current html - need to change to DOM html body

---
+++ background.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+var time = document.body.innerText.split(" ").length / 250.0000;
+ document.getElementById("time-number").innerHTML=time + "min read";
+
+});
